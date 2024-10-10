22f4a9398d2539d87921c7fef81a814604ff5948
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-10-10 15:08:27 -0600

    Add files via upload

---
+++ diccionarios 1.1.py
@@ -0,0 +1,10 @@
+print(" ")
+print(" Arzaba Diaz April 3W 1173")
+#esta linea sirve para almacenar datos en forma de pares clave-valor
+thisdict = { 
+    "marca": "ford",
+  "modelo": "mustang",
+  "año": 1964
+}
+#esta linea lo imprimira
+print(thisdict) 
